f57c2ff60a0978ad837059cf9094328c4f330a8b
Suleman Saleh <your-email@example.com>
2026-05-21 13:51:02 +0500

    push

--- Frontend/ScriptNova-frontend/src/pages/Dashboard.jsx
+++ Frontend/ScriptNova-frontend/src/pages/Dashboard.jsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 
 // import Sidebar from "../components/dashboard/Sidebar"
-import Sidebar from "../components/dashboard/sidebar"
+import Sidebar from "../components/dashboard/Sidebar"
 import Header from "../components/dashboard/Header"
 
 import BlogGenerator from "../components/dashboard/BlogGenerator"
--- Frontend/ScriptNova-frontend/src/pages/Landing.jsx
+++ Frontend/ScriptNova-frontend/src/pages/Landing.jsx
@@ -1,6 +1,6 @@
 // import Navbar from '../components/Navbar';
 import HeroSection from '../components/HeroSection.jsx';
-import Navbar from '../components/NavBar.jsx';
+import Navbar from '../components/Navbar.jsx';
 import FeaturesSection from '../components/FeaturesSection';
 import PricingSection from '../components/PricingSection.jsx';
 import About from '../components/About.jsx';
