2a3effed6c1d67d71e58f2662e4190c43e42f13e
Suleman Saleh <your-email@example.com>
2026-05-21 14:52:01 +0500

    updated ui price section

--- Frontend/ScriptNova-frontend/src/components/PricingSection.jsx
+++ Frontend/ScriptNova-frontend/src/components/PricingSection.jsx
@@ -33,7 +33,7 @@ function PricingSection() {
   };
 
   return (
-    <section id="pricing" className="py-24 bg-gray-950 text-white">
+    <section id="pricing" className="py-24 bg-slate-200  text-black">
       <div className="max-w-6xl mx-auto px-6 text-center">
         <h3 className="text-4xl font-bold mb-16">Simple Pricing</h3>
         {error && (
@@ -41,11 +41,11 @@ function PricingSection() {
             {error}
           </p>
         )}
-        <div className="grid md:grid-cols-2 gap-10">
-          <div className="bg-gray-900 p-10 rounded-3xl">
+        <div className="grid md:grid-cols-2 gap-10 ">
+          <div className="bg-pink-400 p-10 rounded-3xl">
             <h4 className="text-2xl font-semibold">Free</h4>
             <p className="text-4xl font-bold mt-4">$0</p>
-            <ul className="mt-6 space-y-3 text-gray-400">
+            <ul className="mt-6 space-y-3 ">
               <li>5 AI generations / month</li>
               <li>Basic Dashboard</li>
               <li>Email Support</li>
@@ -69,7 +69,7 @@ function PricingSection() {
             <button
               onClick={handleUpgrade}
               disabled={isLoading}
-              className="mt-8 w-full bg-black py-3 rounded-xl font-semibold disabled:cursor-not-allowed disabled:opacity-60"
+              className="text-white mt-8 w-full bg-black py-3 rounded-xl font-semibold disabled:cursor-not-allowed disabled:opacity-60"
             >
               {isLoading ? "Opening Checkout..." : "Upgrade Now"}
             </button>
